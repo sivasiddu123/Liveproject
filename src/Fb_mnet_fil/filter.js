0e385e3512ac687266a881a97948a138af78b1d2
siddu <105799401+sivasiddu123@users.noreply.github.com>
2022-12-02 17:11:24 +0530

    auto

---
+++ src/Fb_mnet_fil/filter.js
@@ -0,0 +1,79 @@
+import {MongoClient} from "mongodb";
+import moment from "moment";
+
+const mClient=async function () {
+    const uri = "mongodb+srv://admin:Adsi2022@arc02.2lutz.mongodb.net/LocalTest?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+  
+    return client;
+};
+
+async function abc(){
+    var twodays=moment().subtract(1,"days").format("YYYY-MM-DD");
+    var today=moment().format("YYYY-MM-DD");
+    var acc_number = process.argv[2];
+    var timez=process.argv[3];
+    var fb=[];
+    var media=[]
+    var shop=[]
+    var all=[]
+const client=await mClient();
+await client.connect();
+
+const facebook=await client.db("LocalTest").collection("fb_Acc").find({"date_start":today,"acc_number":acc_number}).toArray();
+fb.push(...facebook)
+// console.log(facebook.length)
+const mnet=await client.db("LocalTest").collection("Mnet_ABD").find({"date": { $gte:twodays, $lte:today}}).toArray()
+media.push(...mnet)
+if(timez="BST"){
+    shop.push(fb.adset_id===media.adset_id && fb.BSTHour===media.BSTHour && fb.BSTDate===media.BSTDate)
+}else{
+    console.log("notpossible")
+}
+// console.log(shop.length)
+var swipe=shop.map((element)=>{
+    return{
+        acc_number:element.acc_number,
+        time_zone:element.time_zone,
+        spend:element.spend,
+        f_leads:element.leads,
+        cpc:element.cpc,
+        clicks:element.clicks,
+        campaign_name:element.campaign_name,
+        adset_id:element.adset_id,
+        f_impressions:element.impressions,
+        hourly_stats_aggregated_by_advertiser_time_zone:element.hourly_stats_aggregated_by_advertiser_time_zone,
+        date_start:element.date_start,
+        f_hour:element.hour,
+        date:moment(element.date).format('YYYY-MM-DD'),
+        channelName:element.channelName,
+        channelName2:element.channelName2,
+        channelName3:element.adset_id,
+        m_conv_totalClicks:element.totalClicks,
+        estimatedRevenue:element.estimatedRevenue,
+        m_hour:element.m_hour,
+        Profit:(element.estimatedRevenue-element.spend).toFixed(2) ? (element.estimatedRevenue-element.spend).toFixed(2):"0",
+        Margin:element.estimatedRevenue=='0' || element.spend=='0' ? '0' : (((element.estimatedRevenue-element.spend)/element.spend)*100).toFixed(2),
+        CPL:element.spend == '0' || element.leads == '0' ? '0' : (element.spend/element.leads).toFixed(2),
+        Mcpl:isFinite((element.spend/element.totalClicks).toFixed(2)) ? (element.spend/element.totalClicks).toFixed(2) : "0",
+        Rpc:element.estimatedRevenue=='0' || element.clicks=='0'? '0' : (element.estimatedRevenue/element.clicks).toFixed(2) 
+    }
+})
+// console.log(swipe.length)
+all.push(...swipe)
+// console.log(all.length)
+async function slim(){
+    const client=await mClient()
+
+       await client.connect()
+    //    await client.db("LocalTest").collection("filter-fbm").deleteMany({date:today})
+    //    await client.db("LocalTest").collection("filter-fbm").insertMany(all)
+
+       // await client.db("LocalTest").collection("Moment_fb").deleteMany({})
+       console.log("data in DB",all.length)
+       client.close()
+   }
+   await slim()
+}
+abc()
---
+++ src/Mnet/Bidder.js
@@ -0,0 +1,136 @@
+import { xml2json } from "xml-js";
+import axios from "axios";
+import { MongoClient } from "mongodb";
+// import moment from "moment";
+import moment from "moment-timezone";
+
+const mClient = async () => {
+    const uri = "mongodb+srv://admin:Adsi2022@arc02.2lutz.mongodb.net/LocalTest?retryWrites=true&w=majority";
+  
+    const client = new MongoClient(uri);
+  
+    return client;
+  };
+  
+async function abc(){
+  var today1= moment().format('YYYY-MM-DD')
+  var yesterday = moment().subtract(1, "days").format("MM/DD/YYYY")
+    var today = moment().format("MM/DD/YYYY")
+  // var today="11/30/2022"
+  // var yesterday="11/30/2022"
+  var dd=[];
+async function sum(){
+    var pages=[]
+    // var yesterday = moment().subtract(1, "days").format("MM/DD/YYYY")
+    // var today = moment().format("MM/DD/YYYY")
+    const response =await axios.get(`https://pubconsole.media.net/api/reports/v1/hourly-channel-wise?customer_guid=CF6FF673-0F7A-44BF-8F26-DC2CE5878740&customer_key=8CU2R1JY7&from_date=${yesterday}&to_date=${today}&page_size=5000&page_number=1`)
+    
+    // console.log("res", response.data)
+    var result = xml2json(response.data, { compact: true, ignoreComment: true, spaces: 4 });
+    var json = JSON.parse(result);
+    var siddu=json.reportStats.statsData.reportItem
+    // console.log("str", json.reportStats._attributes.totalPages)
+    var len = parseInt(json.reportStats._attributes.totalPages)
+    // console.log(json)
+    pages.push(...siddu)
+    // console.log(pages.length)      
+
+    for(var i=2;i<=len;i++){
+        const response2 =await axios.get(`https://pubconsole.media.net/api/reports/v1/hourly-channel-wise?customer_guid=CF6FF673-0F7A-44BF-8F26-DC2CE5878740&customer_key=8CU2R1JY7&from_date=${yesterday}&to_date=${today}&page_size=5000&page_number=${i}`)
+
+        var result = xml2json(response2.data, { compact: true, ignoreComment: true, spaces: 4 });
+        var json = JSON.parse(result);
+        var siddu2=json.reportStats.statsData.reportItem
+        pages.push(...siddu2)
+    }
+    // console.log(pages)
+    var mnet2 = pages.map((element) => {
+    const ST = new Date(element._attributes.date);
+    const UTCTime = new Date(
+      Date.UTC(
+        ST.getFullYear(),
+        ST.getMonth(),
+        ST.getDate(),
+        ST.getHours(),
+        ST.getMinutes(),
+        ST.getSeconds()
+      )
+    );
+    const BST_time = moment(UTCTime).tz("Europe/London");
+    const EDT_time = moment(UTCTime).tz("America/New_York");
+    const GMT_time = moment(UTCTime).tz("Africa/Accra");
+    const IST_time = moment(UTCTime).tz("Asia/Kolkata");
+    const MST_time = moment(UTCTime).tz("America/Dawson");
+    const PDT_time = moment(UTCTime).tz("America/Los_Angeles");
+    const CDT_time = moment(UTCTime).tz("America/Chicago");
+        return {
+          date:moment(element._attributes.date).format('YYYY-MM-DD'),
+          name:"Bidder",
+          delet_date:today1,
+          m_hour:moment(element._attributes.date).format('HH'),
+          channelName:element._attributes.channelName,
+          channelName2:element._attributes.channelName2,
+          adset_id:element._attributes.channelName3,
+          impressions:element._attributes.impressions,
+          totalClicks:element._attributes.totalClicks,
+          estimatedRevenue:element._attributes.estimatedRevenue.slice(1,5),
+          // CDTDate:moment(element._attributes.date).tz("America/Chicago").format("DD"),
+          // CDTMonth:moment(element._attributes.date).tz("America/Chicago").format("MM"),
+          // CDTHour:moment(element._attributes.date).tz("America/Chicago").format("HH"),
+      EDT_timestamp: EDT_time.format("YYYY-MM-DD"),
+      EDTHour: moment(EDT_time).format("HH"),
+      EDTDate: moment(EDT_time).format("DD"),
+      EDTMonth: moment(EDT_time).format("MM"),
+      BST_timestamp: BST_time.format("YYYY-MM-DD"),
+      BSTHour: moment(BST_time).format("HH"),
+      BSTDate: moment(BST_time).format("DD"),
+      BSTMonth: moment(BST_time).format("MM"),
+      MST_timestamp: MST_time.format("YYYY-MM-DD"),
+      MSTHour: moment(MST_time).format("HH"),
+      MSTDate: moment(MST_time).format("DD"),
+      MSTMonth: moment(MST_time).format("MM"),
+      GMT_timestamp: GMT_time.format("YYYY-MM-DD"),
+      GMTHour: moment(GMT_time).format("HH"),
+      GMTDate: moment(GMT_time).format("DD"),
+      GMTMonth: moment(GMT_time).format("MM"),
+      UTC_timestamp: moment(UTCTime).format("YYYY-MM-DD"),
+      UTCHour: moment(UTCTime).format("HH"),
+      UTCDate: moment(UTCTime).format("DD"),
+      UTCMonth: moment(UTCTime).format("MM"),
+      PDT_timestamp: PDT_time.format("YYYY-MM-DD"),
+      PDTHour: moment(PDT_time).format("HH"),
+      PDTDate: moment(PDT_time).format("DD"),
+      PDTMonth: moment(PDT_time).format("MM"),
+      IST_timestamp: IST_time.format("YYYY-MM-DD"),
+      ISTHour: moment(IST_time).format("HH"),
+      ISTDate: moment(IST_time).format("DD"),
+      ISTMonth: moment(IST_time).format("MM"),
+      CDT_timestamp: CDT_time.format("YYYY-MM-DD"),
+      CDTHour: moment(CDT_time).format("HH"),
+      CDTDate: moment(CDT_time).format("DD"),
+      CDTMonth: moment(CDT_time).format("MM"),
+        }
+        
+        
+    })
+  // console.log("shoe",mnet2)
+  dd.push(...mnet2)
+}
+await sum();
+// console.log(dd)
+async function db(){
+  const client = await mClient()
+          //    console.log("slope", slope)
+             await client.connect()
+             await client.db("LocalTest").collection("Mnet_ABD").deleteMany({name:"Bidder",delet_date:today1});
+            // //  moment_mnet
+          //   {name:"Adsi",delet_date:today}
+             
+             await client.db("LocalTest").collection("Mnet_ABD").insertMany(dd)
+          //   const data = await client.db("LocalTest").collection("sope").find({}).toArray()
+            console.log("data in DB",dd.length)
+            client.close()
+}
+await db()
+}
+abc()
